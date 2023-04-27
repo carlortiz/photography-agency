4e232085d9e4fca4f025978466b6093edf0012f3
carlortiz <mr.carlortiz@gmail.com>
2023-04-27 11:52:18 -0700

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { useState, useEffect } from 'react';
 import Navbar from "./Navbar"
-import UserDisplay from "./UserDisplay";
+import UserCard from "./UserCard";
 
 function App(){
+    const[value, setValue] = useState(null);
+
+    function handleGetUsersClick() {
+        setValue(true);
+    }
+
+    const [users, setUsers] = useState([])
+
+    const fetchUserData = () => {
+        fetch("https://reqres.in/api/users?page=1")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            setUsers(data)
+        })
+    }
+
     return (
         <div>
-            <div><Navbar></Navbar></div>
-            <div><UserDisplay></UserDisplay></div>
+            <div><Navbar onGetUsersClick={handleGetUsersClick} ></Navbar></div>
+            
+            
         </div>
     );
 }
 
 export default App;
+
+// fetch the data from the link
+// show a loader while the api fetches the data
+// display the data on Cards
+
 
